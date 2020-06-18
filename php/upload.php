28bc5eb0ab3e4912aace15b42baa486eeedfd027
root <root@localhost.localdomain>
2020-06-18 21:32:06 +0530

    Added file upload feature

--- php/upload.php
+++ php/upload.php
@@ -14,13 +14,30 @@
     $sem = $_POST["sem"];
     $branch = $_POST["branch"];
 
+     $f_name=$_FILES['file']['name'];
+     $t_name=$_FILES['file']['tmp_name'];
+     $upload='content';
+     if( is_dir($upload.'/'.$sem.'/'.$branch) === false )
+        {
+            mkdir($upload.'/'.$sem.'/'.$branch);
+        }
+    if( is_dir($upload.'/'.$sem.'/'.$branch.'/'.$sub) === false )
+        {
+            mkdir($upload.'/'.$sem.'/'.$branch.'/'.$sub);
+        }
+     $extend=end((explode(".", $f_name)));
+     $file_url = $upload.'/'.$sem.'/'.$branch.'/'.$sub.'/'.now().$f_name;
+     $fupload=move_uploaded_file($t_name,$file_url);
 
-    $sql = "INSERT INTO posts (created_by, sem, title, post_description, post_subject, post_branch) VALUES ('".$author."', '".$sem."','".$title."','".$desc."','".$sub."','".$branch."')";
+    $sql = "INSERT INTO posts (created_by, sem, title, post_description, post_subject, post_branch, file_url) VALUES ('".$author."', '".$sem."','".$title."','".$desc."','".$sub."','".$branch."','".$file_url."')";
     if ($conn->query($sql) === TRUE) {
     echo "New record created successfully";
     } else {
     echo "Error: " . $sql . "<br>" . $conn->error;
     }
     $conn->close();
+    if($fupload){
+        echo '<script>alert("File uploaded successfully")</script>';
+    }
     header("Location: index.php");
 ?>
