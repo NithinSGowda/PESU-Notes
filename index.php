fa5a0b002db34939b407db77bd49bf7f1457e635
Nithin.S <nithin.s491@gmail.com>
2020-06-20 20:17:45 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php 
     ini_set('display_startup_errors', 1); ini_set('display_errors', 1); error_reporting(-1);
 
-    $PATH = '/var/www/html';
+    $PATH = '/var/www/pesu';
     require_once $PATH.'/libraries/config/config.php';
     session_start();
     $db = getDbInstance();
