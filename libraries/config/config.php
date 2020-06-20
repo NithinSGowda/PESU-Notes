b83c3a4297ad638f70e2172d82b0477d639e5075
Nithin.S <nithin.s491@gmail.com>
2020-06-20 20:16:45 +0530

    Update config.php

--- libraries/config/config.php
+++ libraries/config/config.php
@@ -2,7 +2,7 @@
 error_reporting(E_ALL);
 ini_set('display_errors', 'On');
 define('BASE_PATH', dirname(dirname(__FILE__)));
-define("PARENT",     "/var/www/html");
+define("PARENT",     "/var/www/pesu");
 define('APP_FOLDER', 'pesu_notes');
 define('CURRENT_PAGE', basename($_SERVER['REQUEST_URI']));
 
@@ -10,9 +10,9 @@ require_once BASE_PATH . '/lib/MysqliDb/MysqliDb.php';
 require_once BASE_PATH . '/helpers/helpers.php';
 
 define('DB_HOST', "localhost");
-define('DB_USER', "root");
-define('DB_PASSWORD', "9419002492");
-define('DB_NAME', "pesu_notese");
+define('DB_USER', "pesu");
+define('DB_PASSWORD', "pesunith");
+define('DB_NAME', "PESU");
 
 function getDbInstance() {
 	return new MysqliDb(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME);
