921c11103cccdeb041fd52c66808eb220644c573
Qazi Amaan <qazi9amaan@gmail.com>
2020-07-02 14:31:26 +0530

    completed the user page

---
+++ test.php
@@ -0,0 +1,10 @@
+<?php include 'includes/header.php'; ?>
+<div class="container">
+    <div class="form">
+        <form action="https://www.pesuacademy.com/Academy/getStudentClassInfo.php" method="POST">
+        <button type="submit">Sub</button>
+        <input type="text" name="loginId" value="">
+        </form>
+    </div>
+</div>   
+<?php include 'includes/scripts.php'; ?>
