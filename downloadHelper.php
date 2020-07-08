3fc2e39202827e0acc8caecd60d9434f53fb46b0
Nithin.S <nithin.s491@gmail.com>
2020-07-08 18:19:17 +0530

    Fixed download isssue
    Dated 8/7/20

--- downloadHelper.php
+++ downloadHelper.php
@@ -1,13 +1,10 @@
 <?php
-    ini_set('display_startup_errors', 1); ini_set('display_errors', 1); error_reporting(-1);
     require_once '/var/www/pesu/libraries/config/config.php';
-    session_start();
-    if(!isset($_SESSION['user_logged_in'])){
-        header("Location: /signIn.php");
-      }
-    $download = $_SESSION["post_data"]['mcrypt_encrypt'];
+	session_start();
+	if(!isset($_SESSION['user_logged_in'])){
+      header("Location: signIn.php");
+    }
     $id = decrypt(filter_input(INPUT_POST, 'mcrypt_encrypt'));
-	if($download ===$id ){
     if ($id && $_SERVER['REQUEST_METHOD'] == 'POST') {
         $db3 = getDbInstance();
         $db3->where('user_id', $_SESSION['user_id']);
@@ -31,7 +28,5 @@
         }  
         else
             echo 'Insufficient Balance';
-    }}else{
-     header('Location: download.php');
     }
 ?>
