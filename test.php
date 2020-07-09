b930afb57510f2422678151bca93f14bb6ae3780
NithinSGowda <nithin.s491@gmail.com>
2020-07-09 21:21:51 +0530

    Added Thumbnail generation

--- test.php
+++ test.php
@@ -1,10 +1,58 @@
-<?php include 'includes/header.php'; ?>
-<div class="container">
-    <div class="form">
-        <form action="https://www.pesuacademy.com/Academy/getStudentClassInfo.php" method="POST">
-        <button type="submit">Sub</button>
-        <input type="text" name="loginId" value="">
-        </form>
-    </div>
-</div>   
-<?php include 'includes/scripts.php'; ?>
+<?php 
+
+function is_pdf ( $file ) { 
+	$file_content = file_get_contents( $file ); 
+	
+	if ( preg_match( "/^%PDF-[0-1]\.[0-9]+/", $file_content ) ) { 
+		return true; 
+	} 
+	else { 
+		return false; 
+	} 
+} 
+
+function create_preview ( $file ) { 
+	$output_format = "jpeg"; 
+	$antialiasing = "4"; 
+	$preview_page = "1"; 
+	$resolution = "120"; 
+	$output_file = $_GET["file"].".jpg"; 
+
+	$exec_command = "gs -dSAFER -dBATCH -dNOPAUSE -sDEVICE=" . $output_format . " "; 
+	$exec_command .= "-dTextAlphaBits=". $antialiasing . " -dGraphicsAlphaBits=" . $antialiasing . " "; 
+	$exec_command .= "-dFirstPage=" . $preview_page . " -dLastPage=" . $preview_page . " "; 
+	$exec_command .= "-r" . $resolution . " "; 
+	$exec_command .= "-sOutputFile=" . $output_file . " '" . $file . "'"; 
+
+	echo "Executing command...\n"; 
+	exec( $exec_command, $command_output, $return_val ); 
+	
+	foreach( $command_output as $line ) { 
+		echo $line . "\n"; 
+	} 
+
+	if ( !$return_val ) { 
+        echo "Preview created successfully!!\n";
+        echo "<br><img src=\"content/Profile.pdf.jpg\">"; 
+	} 
+	else { 
+		echo "Error while creating the preview.\n"; 
+	} 
+} 
+
+function __main__() { 
+
+	$input_file = $_GET["file"]; 
+
+	if ( is_pdf( $input_file ) ) { 
+		// Create preview for the pdf 
+		create_preview( $input_file ); 
+	} 
+	else { 
+		echo "The input file " . $input_file . " is not a valid PDF document.\n"; 
+	} 
+} 
+
+__main__(); 
+	
+?> 
--- upload.php
+++ upload.php
@@ -59,11 +59,13 @@
     $data_to_db['file_url'] = '/'.'content/'.$data_to_db['post_semester'].'/'.$data_to_db['post_branch'].'/'.$data_to_db['post_subject'].'/'.$insertValuesSQL;
 
     $db = getDbInstance();
+    $dbThumb = getDbInstance();
     $last_id = $db->insert('posts', $data_to_db);
 
     if ($last_id)
     {
         $_SESSION['success'] = 'Notes uploaded successfully!';
+        __main__(); 
         header('Location: /upload.php');
     	  exit();
     }
@@ -75,6 +77,53 @@
     }
 }
 
+    function is_pdf ( $file ) { 
+      $file_content = file_get_contents( $file ); 
+      
+      if ( preg_match( "/^%PDF-[0-1]\.[0-9]+/", $file_content ) ) { 
+        return true; 
+      } 
+      else { 
+        return false; 
+      } 
+    } 
+
+    function create_preview ( $file ) { 
+      $output_format = "jpeg"; 
+      $antialiasing = "4"; 
+      $preview_page = "1"; 
+      $resolution = "120"; 
+      $output_file = $data_to_db['file_url'].".jpg"; 
+
+      $exec_command = "gs -dSAFER -dBATCH -dNOPAUSE -sDEVICE=" . $output_format . " "; 
+      $exec_command .= "-dTextAlphaBits=". $antialiasing . " -dGraphicsAlphaBits=" . $antialiasing . " "; 
+      $exec_command .= "-dFirstPage=" . $preview_page . " -dLastPage=" . $preview_page . " "; 
+      $exec_command .= "-r" . $resolution . " "; 
+      $exec_command .= "-sOutputFile=" . $output_file . " '" . $file . "'"; 
+
+      // echo "Executing command...\n"; 
+      exec( $exec_command, $command_output, $return_val );  
+
+      if ( !$return_val ) { 
+        $_SESSION['success'] = 'Notes uploaded successfully!';
+      } 
+      else { 
+        $_SESSION['failure'] = "Error while creating the Thumbnail.\n"; 
+      } 
+    } 
+
+    function __main__() { 
+
+      $input_file = $_GET["file"]; 
+
+      if ( is_pdf( $input_file ) ) { 
+        // Create preview for the pdf 
+        create_preview( $input_file ); 
+      } 
+      else { 
+        $_SESSION['failure'] = "The input file " . $input_file . " is not a valid PDF document.\n"; 
+      } 
+    } 
 ?>
 
 <?php include 'includes/header.php'; ?>
